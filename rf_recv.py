8a6dae36e84d4a8ed5fb53fe0ffee7ec2b4e2b17
AsmerAmen <asmer.amenar@gmail.com>
2019-12-01 13:29:32 +0200

    Test 02.2

--- rf_recv.py
+++ rf_recv.py
@@ -1,9 +1,17 @@
 from ctypes import cdll, c_long, c_int
 from time import sleep
+import RPi.GPIO as GPIO
+
+PIN = 2
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(PIN, GPIO.IN)
+
 rf_lib = cdll.LoadLibrary("rfSniffer.so")
 rfInit = rf_lib.init
 rfValue = rf_lib.getValue
 rfValue.restype = c_int
+
 # print(1)
 if rfInit():
     print('success')
@@ -13,4 +21,4 @@ else:
 rfValue.restype = c_long
 while True:
     print("Recv: :", rfValue())
-    sleep(0.100)
+    sleep(1)  # 1 second
