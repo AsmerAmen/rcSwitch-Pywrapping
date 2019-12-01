b19c5e34307790c91b48e2a242e5e83a6fa62407
AsmerAmen <asmer.amenar@gmail.com>
2019-12-01 13:22:57 +0200

    Test 02

--- rf_recv.py
+++ rf_recv.py
@@ -1,14 +1,16 @@
 from ctypes import cdll, c_long, c_int
-
+from time import time
 rf_lib = cdll.LoadLibrary("rfSniffer.so")
 rfInit = rf_lib.init
 rfValue = rf_lib.getValue
 rfValue.restype = c_int
-print(1)
+# print(1)
 if rfInit():
     print('success')
 else:
     print('7mada')
-print(2)
+# print(2)
 rfValue.restype = c_long
-print(rfValue())
+while True:
+    print("Recv: :", rfValue())
+    time.sleep(0.100)
