cd66c2c63da7acdb1562c3d3ec065fedb9a0f5cd
AsmerAmen <asmer.amenar@gmail.com>
2019-12-01 13:06:45 +0200

    Test 01.4

--- rf_recv.py
+++ rf_recv.py
@@ -3,6 +3,8 @@ from ctypes import cdll, c_long
 rf_lib = cdll.LoadLibrary("rfSniffer.so")
 rfInit = rf_lib.init
 rfValue = rf_lib.getValue
+print(1)
 rfInit()
+print(2)
 rfValue.restype = c_long
 print(rfValue())
